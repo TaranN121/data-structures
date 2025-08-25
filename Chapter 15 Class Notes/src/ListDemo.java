04c08cbd82cf835ddc5450a0c3a9b2794bd483f6
TaranN121 <Taran.k.Nathan@gmail.com>
2025-08-25 13:16:59 -0500

    lebron Linked list notes

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,30 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /*the addLast method can be used to populate a list */
+        LinkedList<String> staff = new LinkedList <>();
+        staff.addLast("Lebron");
+        staff.addLast("Dwyane");
+        staff.addLast("Kyrie");
+        staff.addLast("Anthony");
+
+        System.out.println(staff);
+
+        //the list is currently Lebron, Dwyane, Kyrie, Anthony
+        /*
+         * the listIterator method creates a new list iterator
+         * that is positioned at the head of the list
+         * the | is used to represent the iterator position
+         */
+
+         ListIterator<String> iterator = staff.listIterator();  //  |Lebron, Dwyane, Kyrie, Anthony
         
+         /* the next method advances the iterator OVER the next element in the list */
+         iterator.next(); //Lebron,| Dwyane, Kyrie, Anthony
+
+         /* The next method returns the element that the iterator passes over */
+         String nba = iterator.next();
+         System.out.println(nba); // prints Dwyane
+
     }
 }
