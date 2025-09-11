f4758ad302cf02ca63e114994a518fd2f9bf5edb
TaranN121 <Taran.k.Nathan@gmail.com>
2025-09-11 12:47:04 -0500

    Update MapDemo.java

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,28 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /* the Map interface is generic 
+         * the first type given is he key
+         * the second type given is the value
+         */
+        Map<String, Color> favColors = new HashMap<>();
+        
+        //add elements to the map using put
+        //cant have two keys that are duplicates. It will replace the value.
+        favColors.put("Sreeram", Color.GREEN);
+        favColors.put("Kaitlyn", Color.GREEN);
+        favColors.put("Lebron", Color.YELLOW);
+        favColors.put("Nimai", Color.BLACK);
+
+        //create a set of the keys in a map
+
+        Set<String> keys = favColors.keySet();
+        for (String key: keys){
+            //[name] ([hasCode]): [color]
+            System.out.println(key + "("+key.hashCode()+"): "+favColors.get(key));
+        }
+
+
+
     }
 }
