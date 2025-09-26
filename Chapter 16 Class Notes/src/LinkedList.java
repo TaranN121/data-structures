81cfc642108d7d7a363df9e7563bc6cd11462e9b
TaranN121 <Taran.k.Nathan@gmail.com>
2025-09-26 13:17:40 -0500

    asd

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -42,7 +43,14 @@ public Object getfirst(){
         @return the removed element
     */
 
+public Object removeFirst(){
+    if (this.first==null)
+        throw new NoSuchElementException();
 
+    Object element = this.first.data;
+    this.first = this.first.next;
+    return element;
+}
 
 
 
@@ -50,7 +58,12 @@ public Object getfirst(){
         Adds an element to the front of the linked list.
         @param element the element to add
     */
-
+public void addFirst(Object element){
+    Node newNode = new Node();
+    newNode.data = element;
+    newNode.next = this.first;
+    this.first = newNode;
+}
 
 
 
@@ -59,7 +72,10 @@ public Object getfirst(){
         Returns an iterator for iterating through this list.
         @return an iterator for iterating through this list
     */
-
+public ListIterator listIterator()
+{
+    return new LinkedListIterator();
+}
 
 
 
@@ -67,28 +83,46 @@ public Object getfirst(){
     //Class Node
     // Node is static because it dpes mpt need tp access amything in the linkedlist
     //The  object will store information not interact
-    static class Node (
+    static class Node {
         public Node next;
         public Object data;
-    )
+    }
 
 
-    class LinkedListIterator //implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
       //private data
-
+      private Node position;
+      private Node previous;
+      private boolean isafterNext;
 
         /**
             Constructs an iterator that points to the front
             of the linked list.
         */
-
+    public LinkedListIterator(){
+        position = null;
+        previous = null;
+        isafterNext = false;
+    }
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
-
+    public Object next(){
+        if (!hasNext()){
+            throw new NoSuchElementException();
+        }
+        previous = position;
+        if (position == null){
+            position = first;
+        } else {
+            position = position.next;
+        }
+        isafterNext = true;
+        return position.data;
+    }
 
 
 
@@ -97,7 +131,14 @@ public Object getfirst(){
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
-
+ public boolean hasNext(){
+    //check if list is empty
+    if (position == null){
+        return first != null;
+    }
+    //the iterator has moved so check the next node
+    return position.next != null;
+ }
 
         /**
             Adds an element before the iterator position
@@ -105,7 +146,23 @@ public Object getfirst(){
             @param element the element to add
         */
 
+public void add(Object element){
+    //check if iterator at beginning of the list
+    if (position == null){
+        addFirst(element);
+        position = first;
+    }
 
+    else{
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = position.next;
+
+        position.next = newNode;
+        position = newNode;
+    }
+    isafterNext = false;
+}
 
 
 
@@ -115,6 +172,19 @@ public Object getfirst(){
             only be called after a call to the next() method.
         */
 
+public void remove(){
+    if(!isafterNext){
+        throw new IllegalStateException();
+    }
+    if (position == first){
+        removeFirst();
+        position = null;
+    } else {
+        previous.next = position.next;
+        position = previous;
+    }
+    isafterNext = false;
+}
 
 
 
@@ -126,8 +196,26 @@ public Object getfirst(){
             @param element the element to set
         */
 
+public void set(Object element){
+    if (!isafterNext){
+        throw new IllegalStateException();
+    }
+    position.data = element;
+    //we dont have to change isafterNext because the structure of the list hasnt changed 
+}
 
-
-
+public String toString(){
+    ListIterator iterator = listIterator();
+    String result = "[";
+    while (iterator.hasNext()){
+        Object element = iterator.next();
+        result += element;
+        if (iterator.hasNext()){
+            result += ", ";
+        }
+    }
+    result += "]";
+    return result;
     }//LinkedListIterator
 }//LinkedList
+}
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,14 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList list = new LinkedList();
+        list.addFirst("C");
+        list.addFirst("B");
+        list.addFirst("A");
+        System.out.println(list);
+        System.out.println("Removed: " + list.removeFirst());
+        System.out.println(list);
+        System.out.println("First element: " + list.getfirst());
+        System.out.println(list);
     }
 }
